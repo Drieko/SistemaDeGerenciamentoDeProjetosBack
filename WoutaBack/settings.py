e91b2131a14c397b1b1909714ff10787e807ac4a
Drieko <driekodev@gmail.com>
2024-11-04 20:38:10 -0300

    Arrumando a database, docker  e .env

--- WoutaBack/settings.py
+++ WoutaBack/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import dj_database_url
+import os 
+
+DB_URL = config('DB_URL', default=os.environ.get('DB_URL', None))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +81,8 @@ WSGI_APPLICATION = 'WoutaBack.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.config(
+    default=DB_URL)
 }
 
 
