151d867093e34f95e9c27a2576c861a8a63e41ed
Drieko <driekodev@gmail.com>
2024-12-09 19:11:56 -0300

    Coloqueii o url correto do front atual

--- WoutaBack/settings.py
+++ WoutaBack/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://sistemadegerenciamentodeprojetosfront.onrender.com",
+    "https://woutafront.onrender.com",
     "http://127.0.0.1:8000",
     
 
